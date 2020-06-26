4d685c72ca24c42d9358b9f7fbb3efc0f03d6b7e
RemigiuszDrobinski <curiosisciekawski@gmail.com>
2020-06-26 21:55:48 +0200

    Add res / Add NuGet

---
+++ Projekt/Projekt/DAL/DBConnection.cs
@@ -0,0 +1,48 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projekt.DAL
+{
+    class DBConnection
+    {
+        private static MySqlConnectionStringBuilder stringBuilder;
+        private static string Nickname
+        {
+            get;
+            set;
+        } = "root";
+
+        private static string Password
+        {
+            get;
+            set;
+        } = "";
+
+        public static DBConnection Instance
+        {
+            get => new DBConnection();
+        }
+
+        public MySqlConnection Connection => new MySqlConnection(stringBuilder.ToString());
+
+        private DBConnection()
+        {
+            stringBuilder = new MySqlConnectionStringBuilder
+            {
+                UserID = Nickname,
+                Password = Password,
+                Server = "localhost",
+                Database = "project",
+                Port = 3306
+            };
+        }
+
+        public static void Login(string user, string password)
+        {
+            Nickname = user;
+            Password = password;
+        }
+    }
+}
