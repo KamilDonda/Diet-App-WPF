3bb7c9ee92c689105e910ade01f91483acd9e828
RemigiuszDrobinski <curiosisciekawski@gmail.com>
2020-06-28 10:50:15 +0200

    Add QueryManager class

---
+++ Projekt/Projekt/DAL/QueryManager.cs
@@ -0,0 +1,20 @@
+﻿using Projekt.DAL.Entities;
+using Projekt.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projekt.DAL
+{
+    class QueryManager
+    {
+        public List<Ingredients> Ingredients
+        {
+            get => IngredientsRepos.GetAllIngredients();
+        }
+        public List<Meals> Meals
+        {
+            get => MealsRepos.GetAllMeals();
+        }
+    }
+}
